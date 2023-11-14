35c8e78c84fc5dc804c134b6e348e71b964069e7
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2023-11-14 13:46:41 +0100

    qwe

---
+++ robottek/main.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int pik() {
+    int test = 123;
+};  
+
+
+class testClass {
+    public:
+        int testNum;
+        string testStr;
+};
+
+class users {
+    public:
+        int age;
+        string firstName;
+        string lastName;
+        string homeAdress;
+};
+
+int main() {
+    testClass testObj;
+    testObj.testNum = 69;
+    testObj.testStr = "Hello, World!";
+
+    cout << testObj.testNum;
+    cout << "\n";
+    cout << testObj.testStr;
+    cout << "\n";
+    cout << "\n";
+
+    
+    users user1;
+    user1.age = 17;
+    user1.firstName = "Jeppe";
+    user1.lastName = "Bøgeskov";
+    user1.homeAdress = "Alpeviolvej 3";
+
+    users user2;
+    user2.age = 16;
+
+
+    cout << user1.age;
+    cout << "\n";
+    cout << user1.firstName + " " + user1.lastName;
+    cout << "\n";
+    cout << user1.homeAdress;
+    cout << "\n";
+    cout << "\n";
+
+    cout << user2.age;
+    
+    return 0; 
+}
