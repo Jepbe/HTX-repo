87e314c0107b43a308ea36f6f898de28dcc941a1
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2023-11-23 13:27:04 +0100

    qwe

--- wokwi_test.cpp
+++ wokwi_test.cpp
@@ -14,24 +14,24 @@ void setup() {
   lights led_3;
   led_3.led = 13;
 
-  pinMode(led_1.led, OUTPUT);  
-  pinMode(led_2.led, OUTPUT);
-  pinMode(led_3.led, OUTPUT);
+  //pinMode(led_1.led, OUTPUT);  
+  //pinMode(led_2.led, OUTPUT);
+  //pinMode(led_3.led, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(led_1.led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(400);                       // wait for a second
-  digitalWrite(led_1.led, LOW);    // turn the LED off by making the voltage LOW
-  delay(400);  
-  digitalWrite(led_2.led, HIGH);    // turn the LED off by making the voltage LOW
-  delay(400); 
-  digitalWrite(led_2.led, LOW);    // turn the LED off by making the voltage LOW
-  delay(400); 
-  digitalWrite(led_3.led, HIGH);    // turn the LED off by making the voltage LOW
-  delay(400); 
-  digitalWrite(led_3.led, LOW);    // turn the LED off by making the voltage LOW
-  delay(400);  
+  // digitalWrite(led_1.led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // delay(400);                       // wait for a second
+  // digitalWrite(led_1.led, LOW);    // turn the LED off by making the voltage LOW
+  // delay(400);  
+  // digitalWrite(led_2.led, HIGH);    // turn the LED off by making the voltage LOW
+  // delay(400); 
+  // digitalWrite(led_2.led, LOW);    // turn the LED off by making the voltage LOW
+  // delay(400); 
+  // digitalWrite(led_3.led, HIGH);    // turn the LED off by making the voltage LOW
+  // delay(400); 
+  // digitalWrite(led_3.led, LOW);    // turn the LED off by making the voltage LOW
+  // delay(400);  
 }
